62f30190d4c1bbc45d94c0a173e973ef77147cd0
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2024-01-26 13:13:19 +0900

    Replace spaces with tabs

--- MonmusuPlayer/win_media_player.h
+++ MonmusuPlayer/win_media_player.h
@@ -11,40 +11,46 @@
 class CMediaPlayerNotify : public IMFMediaEngineNotify
 {
 public:
-    STDMETHODIMP QueryInterface(REFIID riid, void** ppv) {
-        if (riid == __uuidof(IMFMediaEngineNotify)) {
-            *ppv = static_cast<IMFMediaEngineNotify*>(this);
-        }
-        else {
-            *ppv = nullptr;
-            return E_NOINTERFACE;
-        }
-        AddRef();
-        return S_OK;
-    }
-    STDMETHODIMP EventNotify(DWORD Event, DWORD_PTR param1, DWORD param2) {
-        if (Event == MF_MEDIA_ENGINE_EVENT_NOTIFYSTABLESTATE) {
-            ::SetEvent(reinterpret_cast<HANDLE>(param1));
-        }
-        else {
-            OnMediaEngineEvent(Event, param1, param2);
-        }
-        return S_OK;
-    }
+	STDMETHODIMP QueryInterface(REFIID riid, void** ppv)
+	{
+		if (riid == __uuidof(IMFMediaEngineNotify))
+		{
+			*ppv = static_cast<IMFMediaEngineNotify*>(this);
+		}
+		else
+		{
+			*ppv = nullptr;
+			return E_NOINTERFACE;
+		}
+		AddRef();
+		return S_OK;
+	}
+	STDMETHODIMP EventNotify(DWORD Event, DWORD_PTR param1, DWORD param2)
+	{
+		if (Event == MF_MEDIA_ENGINE_EVENT_NOTIFYSTABLESTATE)
+		{
+			::SetEvent(reinterpret_cast<HANDLE>(param1));
+		}
+		else
+		{
+			OnMediaEngineEvent(Event, param1, param2);
+		}
+		return S_OK;
+	}
 	STDMETHODIMP_(ULONG) AddRef() { return ::InterlockedIncrement(&m_lRef); }
-    STDMETHODIMP_(ULONG) Release()
-    {
-        LONG lRef = ::InterlockedDecrement(&m_lRef);
-        if (!lRef)delete this;
-        return lRef;
-    }
+	STDMETHODIMP_(ULONG) Release()
+	{
+		LONG lRef = ::InterlockedDecrement(&m_lRef);
+		if (!lRef)delete this;
+		return lRef;
+	}
 
-    void SetPlayer(void* arg);
-
+	void SetPlayer(void* arg);
 private:
-    LONG m_lRef = 0;
-    void* m_pPlayer = nullptr;
-    void OnMediaEngineEvent(DWORD Event, DWORD_PTR param1, DWORD param2);
+	LONG m_lRef = 0;
+	void* m_pPlayer = nullptr;
+
+	void OnMediaEngineEvent(DWORD Event, DWORD_PTR param1, DWORD param2);
 };
 
 class CMediaPlayer
@@ -52,44 +58,44 @@ class CMediaPlayer
 public:
 	CMediaPlayer(HWND hWnd);
 	~CMediaPlayer();
-    bool SetFiles(const std::vector<std::wstring>& filePaths);
-    bool Play();
-    void Next();
-    void Back();
-    BOOL SwitchLoop();
-    BOOL SwitchMute();
-    double GetCurrentVolume();
-    double GetCurrentRate();
-    bool SetCurrentVolume(double dbVolume);
-    bool SetCurrentRate(double dbRate); 
-    void AutoNext();
-    void SwitchSizeLore(bool bBarHidden);
-    void ResizeWindow();
-    void SetDisplayArea(RECT *rect);
-    void ResetZoom();
-    void UpScale();
-    void DownScale();
+	bool SetFiles(const std::vector<std::wstring>& filePaths);
+	bool Play();
+	void Next();
+	void Back();
+	BOOL SwitchLoop();
+	BOOL SwitchMute();
+	double GetCurrentVolume();
+	double GetCurrentRate();
+	bool SetCurrentVolume(double dbVolume);
+	bool SetCurrentRate(double dbRate); 
+	void AutoNext();
+	void SwitchSizeLore(bool bBarHidden);
+	void ResizeWindow();
+	void SetDisplayArea(RECT *rect);
+	void ResetZoom();
+	void UpScale();
+	void DownScale();
 private:
-    HWND m_hRetWnd = nullptr;
-
+	HWND m_hRetWnd = nullptr;
+	
 	HRESULT m_hrComInit = E_FAIL;
 	HRESULT m_hrMfStart = E_FAIL;
-    CMediaPlayerNotify* m_pmfNotify = nullptr;
+	CMediaPlayerNotify* m_pmfNotify = nullptr;
 	IMFMediaEngineEx* m_pmfEngineEx = nullptr;
-    std::mutex m_mutex;
+	std::mutex m_mutex;
 
-    std::vector<std::wstring> m_media_files;
-    size_t m_nIndex = 0;
+	std::vector<std::wstring> m_media_files;
+	size_t m_nIndex = 0;
 
-    BOOL m_iLoop = FALSE;
-    BOOL m_iMute = FALSE;
+	BOOL m_iLoop = FALSE;
+	BOOL m_iMute = FALSE;
 
-    RECT m_srcRect{};
-    double m_dbScale = 1.0;
-    bool m_bBarHidden = false;
+	RECT m_srcRect{};
+	double m_dbScale = 1.0;
+	bool m_bBarHidden = false;
 
-    void Clear();
-    void ResizeBuffer();
+	void Clear();
+	void ResizeBuffer();
 };
 
 #endif //WIN_MEDIA_PLAYER_H_
